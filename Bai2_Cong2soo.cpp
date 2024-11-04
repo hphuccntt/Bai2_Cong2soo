d49b2e7eb1f1aa06669f66e98810f7e0cd49b353
hphuccntt <phuc54212420014@bachkhoasaigon.edu.vn>
2024-11-04 14:29:09 +0700

    sua bai 2

--- Bai2_Cong2soo.cpp
+++ Bai2_Cong2soo.cpp
@@ -6,7 +6,7 @@ int main()
 	int so1, so2;	// khai bao bien so1 va so2 kieu so nguyen
 	int tong;
 	cout << " Nhap so1 :"; cin >> so1;
-	cout << " Nhap so2 :"; cin >> so2;
+	cout << " Nhap so2 :"; cin >> so2
 	tong = so1 + so2;
 	cout << "tong hai so :" << so1 << "+" << so2 << "=" << tong << endl;
 	cout << "tong hai so :" << so1 << "+" << so2 << "=" << so1 + so2;
